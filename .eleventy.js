9dcc2eff1a1d274e0bf46d0ec76fb15fde006a6a
claudio-delvalle <142037954+claudio-delvalle@users.noreply.github.com>
2024-06-03 19:04:50 -0600

    more progress

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,32 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
+
+  eleventyConfig.addFilter("getDate", function (dateStr) {
+    return new Date(dateStr);
+  });
+
+  eleventyConfig.addFilter("formatDate", function (dateObj) {
+    return dateObj.toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  });
+
+  eleventyConfig.addFilter("getPreviousDay", function (dateObj) {
+    const previousDay = new Date(dateObj); // Create a copy of the original date
+    previousDay.setDate(dateObj.getDate() - 1); // Subtract 1 day
+    return previousDay;
+  });
+
+  eleventyConfig.addFilter("getNextDay", function (dateObj) {
+    const previousDay = new Date(dateObj); // Create a copy of the original date
+    previousDay.setDate(dateObj.getDate() + 1); // Add 1 day
+    return previousDay;
+  });
+
   return {
     dir: {
       input: "src",
