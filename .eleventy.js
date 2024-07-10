a5eb525d46b9c5f7a18c4b73a2009858baeb63e3
claudio-delvalle <142037954+claudio-delvalle@users.noreply.github.com>
2024-07-10 17:54:17 -0600

    improvements

--- .eleventy.js
+++ .eleventy.js
@@ -9,12 +9,41 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter("formatDate", function (date, locale = "en-US") {
     const options = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
+      year: "numeric",
+      month: "long",
+      day: "numeric",
     };
     return new Date(date).toLocaleDateString(locale, options);
   });
+
+  // Function to find previous curation
+  eleventyConfig.addFilter(
+    "getPreviousCuration",
+    function (collection, currentUrl) {
+      const currentIndex = collection.findIndex(
+        (curation) => curation.url === currentUrl
+      );
+      if (currentIndex > 0) {
+        return collection[currentIndex - 1];
+      }
+      return null; // Return null if no previous curation found
+    }
+  );
+
+  // Function to find next curation
+  eleventyConfig.addFilter(
+    "getNextCuration",
+    function (collection, currentUrl) {
+      const currentIndex = collection.findIndex(
+        (curation) => curation.url === currentUrl
+      );
+      if (currentIndex < collection.length - 1) {
+        return collection[currentIndex + 1];
+      }
+      return null; // Return null if no next curation found
+    }
+  );
+  
 
   return {
     dir: {
