37a911dac0f881f53530b591c160be801bc937fb
claudio-delvalle <142037954+claudio-delvalle@users.noreply.github.com>
2024-07-10 12:36:29 -0600

    progress

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,20 @@
-const util = require('util');
+const util = require("util");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
 
-  eleventyConfig.addFilter('console', function(value) {
+  eleventyConfig.addFilter("console", function (value) {
     return util.inspect(value);
-});
+  });
+
+  eleventyConfig.addFilter("formatDate", function (date, locale = "en-US") {
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    };
+    return new Date(date).toLocaleDateString(locale, options);
+  });
 
   return {
     dir: {
